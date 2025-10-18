29206bedf0ea948dbb93505a033ac2375ab6b954
minaboktor2628 <mina26b8@gmail.com>
2025-10-18 17:02:52 -0400

    add latex math

--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -1,13 +1,38 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Showdown from 'showdown';
+	import katex from 'katex';
+	import 'katex/dist/katex.min.css';
+
 	let { data }: { data: PageData } = $props();
-	const converter = new Showdown.Converter();
+
+	// Preserve LaTeX syntax (don’t escape $...$)
+	const converter = new Showdown.Converter({
+		extensions: [
+			() => [
+				{
+					type: 'output',
+					regex: /\$(.+?)\$/g,
+					replace: (match, content) => katex.renderToString(content, { throwOnError: false })
+				},
+				{
+					type: 'output',
+					regex: /\$\$(.+?)\$\$/gs,
+					replace: (match, content) =>
+						katex.renderToString(content, {
+							displayMode: true,
+							throwOnError: false
+						})
+				}
+			]
+		]
+	});
+
 	const postContentHTML = converter.makeHtml(data.post.content);
 </script>
 
 <article class="prose dark:prose-invert">
 	<h1>{data.post.fields.title}</h1>
-	<p>{data.post.fields.date}</p>
+	<p><i>Posted on: {data.post.fields.date}</i></p>
 	{@html postContentHTML}
 </article>
